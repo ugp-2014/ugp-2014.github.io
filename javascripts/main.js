f4452d94255ef51dc2fb09d1173b319a0715b773
山田真充 <vamtan@PS1324.(none)>
2014-09-04 00:01:46 +0900

    New

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,35 @@
 ﻿(function($) {
+    $(window).bind("scroll", function() {
+	// ドキュメントの高さ
+	scrollHeight = $(document).height();
+	// ウィンドウの高さ+スクロールした高さ→ 現在のトップからの位置
+	scrollPosition = $(window).height() + $(window).scrollTop();
+	// フッターの高さ
+	footHeight = $("footer").height();
 	
+	// スクロール位置がフッターまで来たら
+	if ( scrollHeight - scrollPosition  <= footHeight ) {
+		// ページトップリンクをフッターに固定
+		$(".pageTop a").css({"position":"absolute","bottom": "0px"});
+	} else {
+		// ページトップリンクを右下に固定
+		$(".pageTop a").css({"position":"fixed","bottom": "0px"});
+		}
+	});
+	// ページ内リンクをクリックすると
+	$('a[href^=#]').click(function(){
+ 		// スクロールスピード
+ 		var speed = 500;
+ 		// クリックしたリンク先を保存
+		var href= $(this).attr("href");
+		// クリックしたリンク先が#または空のときは
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		// トップへ移動する
+		var position = target.offset().top;
+		// リンク先へスムーズに移動する
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
     //回し続けてみる
     var angle = 0;
     setInterval(function(){
